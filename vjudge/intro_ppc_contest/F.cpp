7f82b96bfd810be0090b3625f635f7310db56902
Arthur Rodrigues <arthurarp97@gmail.com>
2020-04-06 19:08:26 -0300

    ultimo exercicio do contest

---
+++ vjudge/intro_ppc_contest/F.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int sum_elements(int n) 
+{
+    int sum = 0, m;
+
+    while(n > 0)
+    {    
+        m = n % 10;    
+        sum = sum + m;    
+        n = n / 10;    
+    }
+    return sum;
+}
+
+int main(void)
+{
+    int N; cin >> N;
+    int menor = INT_MAX;
+
+    for(int i = 1, j = N - 1; i <= N / 2 and j >= N / 2; ++i and --j)
+    {
+        int soma_a = sum_elements(i);
+        int soma_b = sum_elements(j);
+        int ab = soma_a + soma_b;
+
+        if(ab < menor)
+            menor = ab;
+    }
+
+    cout << menor << endl;
+
+    return 0;
+}
