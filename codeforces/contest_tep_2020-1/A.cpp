25b93443ca9263ca006c89693e64219e34af7e04
Arthur Rodrigues <arthurarp97@gmail.com>
2020-04-06 15:16:45 -0300

    problem a tep_contest 2020.1

---
+++ codeforces/contest_tep_2020-1/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b, c; cin >> a >> b >> c;
+
+    b >= a and c >= a ? cout << "Yes\n" : cout << "No\n";
+
+    return 0;
+}
