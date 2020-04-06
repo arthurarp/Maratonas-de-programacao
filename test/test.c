1e8ae892e8d6a8ac3c02a8def31f9b2d8bcffef6
Arthur Rodrigues <arthurarp97@gmail.com>
2020-04-06 15:49:17 -0300

    entendendo a funcao sync_with_stdio

---
+++ test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int vector[1000000];
+    for(int i = 0; i < 1000000; ++i)
+        scanf("%d", &vector[i]);
+
+    return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+
+    std::ios::sync_with_stdio(false);
+
+    int vector[1000000];
+    for(int i = 0; i < 1000000; ++i)
+        std::cin >> vector[i];
+
+
+    return 0;
+}
