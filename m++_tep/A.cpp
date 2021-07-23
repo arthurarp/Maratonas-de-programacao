005d5f02bb2052cf17d7fb389018faa3ea096e2e
Arthur <arthurarp@hotmail.com>
2021-07-23 15:50:38 -0300

    exercicios

---
+++ m++_tep/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+
+  int soma = 0;
+
+  for (int i = 0; i < 4; ++i)
+  {
+
+    for (int j = 0; j < 13; ++j)
+    {
+      string card;
+      cin >> card;
+
+      cout << card << endl;
+    }
+  }
+
+  return 0;
+}
