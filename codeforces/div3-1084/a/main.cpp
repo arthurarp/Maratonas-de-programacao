987ba3aff82cb6f73fef05b156eea034eda70f5c
Arthur Rodrigues <arthurarp@hotmail.com>
2026-08-22 18:48:59 -0300

    changes
    Signed-off-by: Arthur Rodrigues <arthurarp@hotmail.com>

--- codeforces/div3-1084/a/main.cpp
+++ codeforces/div3-1084/a/main.cpp
@@ -4,17 +4,60 @@ using namespace std;
 
 int main(void)
 {
-  ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 
-  int N; cin >> N;
+    int t; cin >> t;
 
-  if(N == 1)
-    cout << "Hello World\n";
-  else
-  {
-    int a, b; cin >> a >> b;
-    cout << a + b << endl;
-  } 
+    while(t--) {
+
+      int n; cin >> n;
+
+      // if (n == 1) {
+      //   cout << 1 << endl;
+      //   return 0;
+      // }
+
+      vector<int> a(11, 0);
+      for(int i = 1; i <= n; ++i) {
+        cout << "[" << i << "]:" << a[i] << endl;
+
+      }
+
+      int top_int = 0;
+
+      for(int i = 1; i <= n; ++i) {
+
+        int j; cin >> j;
+
+        a[i] = j;
+
+        if(j > top_int) 
+          top_int = j;
+
+      }
+
+      // for(int i = 1; i <= n; ++i) {
+      //   cout << "[" << i << "]:" << a[i] << endl;
+
+      // }
+
+      for(int i = 1; i < top_int; ++i) {
+        cout << "[" << i << "]:" << a[i] << endl;
+        a[i]--;
+
+      }
+
+      int count_winners = 0;
+
+      for(int i = 0; i <= a.size(); ++i) {
+          if (a[i] == 1)
+            count_winners++;
+      }
+
+      cout << count_winners << endl;
+
+
+    }
   
 
 
